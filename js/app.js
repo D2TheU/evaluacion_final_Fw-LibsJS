c008015bf4e0bb6622f593d98da4d732e43afb23
Dueynn Ogaz Siller <dueynn@gmail.com>
2017-11-02 18:47:47 -0600

    Agregar detección de soltar en vecino, y animate si es el de la izquierda.

--- js/app.js
+++ js/app.js
@@ -33,14 +33,14 @@ function createBoardData() {
 function printBoard(board) {
     for (var i = board.length - 1; i > -1; i--) {
         for (var j = 0; j < board[i].length; j++) {
-            $('.col-' + (j + 1)).prepend('<img class="candy-img-' + (i + 1) + '-' + (j + 1) + '" src="image/' + board[i][j] + '.png" class="dulce" />');
-            var topStart = parseInt($('.candy-img-' + (i + 1) + '-' + (j + 1)).css('height')) * -(i + 1);
-            animateCandy($('.candy-img-' + (i + 1) + '-' + (j + 1)), topStart, board.length - i);
+            $('.col-' + (j + 1)).prepend('<img class="candy-img ' + (i + 1) + '-' + (j + 1) + '" src="image/' + board[i][j] + '.png" class="dulce" />');
+            var topStart = parseInt($('.' + (i + 1) + '-' + (j + 1)).css('height')) * -(i + 1);
+            animateCandy($('.' + (i + 1) + '-' + (j + 1)), topStart, board.length - i, i, j);
         }
     }
 }
 
-function animateCandy(candy, topStart, num) {
+function animateCandy(candy, topStart, num, i, j) {
     $(candy).css('opacity', '0');
     var self = this;
     candy.animate({
@@ -52,6 +52,82 @@ function animateCandy(candy, topStart, num) {
             'opacity': 1
         }, 500)
     });
+    $(candy).draggable({
+        start: function() {
+            $('img').css("z-index", "auto")
+            $(this).css("z-index", "2")
+        },
+        revert: function(event, ui) {
+            $(this).data("uiDraggable").originalPosition = {
+                top: 0,
+                left: 0
+            };
+            return !event;
+        }
+    })
+    $(candy).droppable({
+        drop: function(event, ui) {
+            console.log(ui.draggable.attr("class"));
+            var candyMoved = [
+                parseInt(ui.draggable.attr("class")[0]),
+                parseInt(ui.draggable.attr("class")[2])
+            ];
+            var candyDrop = [
+                parseInt($(this).attr("class")[0]),
+                parseInt($(this).attr("class")[2])
+            ];
+            console.log(candyMoved);
+            console.log(candyDrop);
+            var moveTo = [candyDrop[1] - candyMoved[1], candyDrop[0] - candyMoved[0]];
+            if (moveTo[0] == -1 && moveTo[1] == 0) {
+                swapCandy(candyMoved, candyDrop, "left");
+            } else if (moveTo[0] == 1 && moveTo[1] == 0) {
+                swapCandy(candyMoved, candyDrop, "right");
+            } else if (moveTo[0] == 0 && moveTo[1] == -1) {
+                swapCandy(candyMoved, candyDrop, "top");
+            } else if (moveTo[0] == 0 && moveTo[1] == 1) {
+                swapCandy(candyMoved, candyDrop, "bottom");
+            } else {
+                ui.draggable.animate({
+                    top: 0,
+                    left: 0
+                });
+            }
+        }
+    })
+    candy.removeClass((i + 1) + '-' + (j + 1));
+    candy[0].className = ((i + 1) + '-' + (j + 1)) + ' ' + candy[0].className;
+    console.log(candy[0].className);
+}
+
+function swapCandy(candyMoved, candyDrop, side) {
+    var colOrigin = $('.col-' + candyMoved[1])[0].getBoundingClientRect();
+    var colDestination = $('.col-' + candyDrop[1])[0].getBoundingClientRect();
+    switch (side) {
+        case "left":
+            candy1 = $('.' + candyMoved[0] + '-' + candyMoved[1]);
+            candy2 = $('.' + candyDrop[0] + '-' + candyDrop[1]);
+            candy1.animate({
+                top: 0,
+                left: -(colOrigin.left - colDestination.left)
+            });
+            candy2.animate({
+                top: 0,
+                left: (colOrigin.left - colDestination.left)
+            });
+            break;
+        case "right":
+
+            break;
+        case "top":
+
+            break;
+        case "bottom":
+
+            break;
+        default:
+
+    }
 }
 //Document.Ready
 $(function() {
