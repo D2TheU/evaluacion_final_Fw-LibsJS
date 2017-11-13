d99ad87a679e23c537e6944420e8627680a29aef
Dueynn Ogaz Siller <dueynn@gmail.com>
2017-11-13 10:48:42 -0600

    Agregar user-select none a estilos.css Agregar funcionalidad para mover candy por tablero

--- js/app.js
+++ js/app.js
@@ -54,8 +54,8 @@ function animateCandy(candy, topStart, num, i, j) {
     });
     $(candy).draggable({
         start: function() {
-            $('img').css("z-index", "auto")
-            $(this).css("z-index", "2")
+            $('img').css("z-index", "auto");
+            $(this).css("z-index", "2");
         },
         revert: function(event, ui) {
             $(this).data("uiDraggable").originalPosition = {
@@ -67,7 +67,7 @@ function animateCandy(candy, topStart, num, i, j) {
     })
     $(candy).droppable({
         drop: function(event, ui) {
-            console.log(ui.draggable.attr("class"));
+            // console.log(ui.draggable.attr("class"));
             var candyMoved = [
                 parseInt(ui.draggable.attr("class")[0]),
                 parseInt(ui.draggable.attr("class")[2])
@@ -76,8 +76,8 @@ function animateCandy(candy, topStart, num, i, j) {
                 parseInt($(this).attr("class")[0]),
                 parseInt($(this).attr("class")[2])
             ];
-            console.log(candyMoved);
-            console.log(candyDrop);
+            // console.log(candyMoved);
+            // console.log(candyDrop);
             var moveTo = [candyDrop[1] - candyMoved[1], candyDrop[0] - candyMoved[0]];
             if (moveTo[0] == -1 && moveTo[1] == 0) {
                 swapCandy(candyMoved, candyDrop, "left");
@@ -97,37 +97,42 @@ function animateCandy(candy, topStart, num, i, j) {
     })
     candy.removeClass((i + 1) + '-' + (j + 1));
     candy[0].className = ((i + 1) + '-' + (j + 1)) + ' ' + candy[0].className;
-    console.log(candy[0].className);
 }
 
 function swapCandy(candyMoved, candyDrop, side) {
-    var colOrigin = $('.col-' + candyMoved[1])[0].getBoundingClientRect();
-    var colDestination = $('.col-' + candyDrop[1])[0].getBoundingClientRect();
+    var candy1 = $('.' + candyMoved[0] + '-' + candyMoved[1]);
+    var candy2 = $('.' + candyDrop[0] + '-' + candyDrop[1]);
     switch (side) {
         case "left":
-            candy1 = $('.' + candyMoved[0] + '-' + candyMoved[1]);
-            candy2 = $('.' + candyDrop[0] + '-' + candyDrop[1]);
-            candy1.animate({
-                top: 0,
-                left: -(colOrigin.left - colDestination.left)
-            });
-            candy2.animate({
-                top: 0,
-                left: (colOrigin.left - colDestination.left)
-            });
-            break;
         case "right":
-
+            if (candyMoved[0] < 7) {
+                candy1.insertBefore($('.' + (candyDrop[0] + 1) + '-' + candyDrop[1]));
+                candy2.insertBefore($('.' + (candyMoved[0] + 1) + '-' + candyMoved[1]));
+            } else {
+                candy1.insertAfter($('.' + (candyDrop[0] - 1) + '-' + candyDrop[1]));
+                candy2.insertAfter($('.' + (candyMoved[0] - 1) + '-' + candyMoved[1]));
+            }
             break;
         case "top":
-
+            candy1.insertBefore(candy2);
             break;
         case "bottom":
-
+            candy1.insertAfter(candy2);
             break;
-        default:
-
     }
+    candy1.animate({
+        top: 0,
+        left: 0
+    }, 0);
+    candy2.animate({
+        top: 0,
+        left: 0
+    }, 0);
+
+    candy1.removeClass(candyMoved[0] + '-' + candyMoved[1]);
+    candy2.removeClass(candyDrop[0] + '-' + candyDrop[1]);
+    candy1[0].className = candyDrop[0] + '-' + candyDrop[1] + ' ' + candy1[0].className;
+    candy2[0].className = candyMoved[0] + '-' + candyMoved[1] + ' ' + candy2[0].className;
 }
 //Document.Ready
 $(function() {
