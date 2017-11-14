a2c39202b0152bc530bdf4974db1baba46e0483f
Dueynn Ogaz Siller <dueynn@gmail.com>
2017-11-13 18:44:22 -0600

    Agregar checar tablero por candies seguidos Agregar delete candy Agregar refillCandy Agregar addPoins Agregar addToArray Agregar enable y disable drag

--- js/app.js
+++ js/app.js
@@ -12,9 +12,10 @@ function color(elemento) {
 //Create random board
 function createBoardData() {
     var board = [];
-    for (var i = 0; i < 7; i++) {
+    var boardWidth = $('[class*="col-"]').length;
+    for (var i = 0; i < boardWidth; i++) {
         var row = [];
-        for (var j = 0; j < 7; j++) {
+        for (var j = 0; j < boardWidth; j++) {
             do {
                 var num = Math.floor(Math.random() * 4) + 1;
                 var columnRepeat = false;
@@ -65,16 +66,14 @@ function animateCandy(candy, topStart, num, i, j) {
                 left: 0
             };
             return !event;
-        }
+        },
+        revertDuration: 200
     });
     $(candy).droppable({
         drop: function(event, ui) {
-            $('img[class*="candy-img"]').draggable({
-                disabled: true
-            });
+            disableDrag();
             $(this).attr('data-x', $(this)[0].getBoundingClientRect().left);
             $(this).attr('data-y', $(this)[0].getBoundingClientRect().top);
-            addMovement();
             var candyMoved = [
                 parseInt(ui.draggable.attr("class")[0]),
                 parseInt(ui.draggable.attr("class")[2])
@@ -96,6 +95,8 @@ function animateCandy(candy, topStart, num, i, j) {
                 ui.draggable.animate({
                     top: 0,
                     left: 0
+                }, 200, function() {
+                    enableDrag();
                 });
             }
         }
@@ -105,6 +106,7 @@ function animateCandy(candy, topStart, num, i, j) {
 }
 
 function swapCandy(candyMoved, candyDrop, side) {
+    addMovement();
     var candy1 = $('.' + candyMoved[0] + '-' + candyMoved[1]);
     var candy2 = $('.' + candyDrop[0] + '-' + candyDrop[1]);
     var difX = parseInt($('.' + candyDrop[0] + '-' + candyDrop[1]).attr("data-x") - $('.' + candyMoved[0] + '-' + candyMoved[1]).attr("data-x"));
@@ -112,7 +114,7 @@ function swapCandy(candyMoved, candyDrop, side) {
     switch (side) {
         case "left":
         case "right":
-            if (candyMoved[0] < 7) {
+            if (candyMoved[0] < $('[class*="col-"]').length) {
                 candy1.insertBefore($('.' + (candyDrop[0] + 1) + '-' + candyDrop[1]));
                 candy2.insertBefore($('.' + (candyMoved[0] + 1) + '-' + candyMoved[1]));
             } else {
@@ -134,7 +136,7 @@ function swapCandy(candyMoved, candyDrop, side) {
         $(this).animate({
             left: 0,
             top: 0
-        }, 250);
+        }, 200);
     });
     candy2.animate({
         left: 0 + difX,
@@ -144,7 +146,7 @@ function swapCandy(candyMoved, candyDrop, side) {
             left: 0,
             top: 0
         }, 200, function() {
-            $('img[class*="candy-img"]').draggable("enable");
+            checkBoard(1);
         });
     });
 
@@ -153,10 +155,137 @@ function swapCandy(candyMoved, candyDrop, side) {
     candy1[0].className = candyDrop[0] + '-' + candyDrop[1] + ' ' + candy1[0].className;
     candy2[0].className = candyMoved[0] + '-' + candyMoved[1] + ' ' + candy2[0].className;
 }
-// $('img[class*="candy-img"]').draggable({ disabled: true });
-// $('img[class*="candy-img"]').draggable("enable");
+
+function checkBoard(mult) {
+    disableDrag();
+    var complete = [];
+    var boardWidth = $('[class*="col-"]').length;
+    for (var col = 1; col <= boardWidth; col++) {
+        var countColumn = 1,
+            countRow = 1,
+            imgColumn = 'start',
+            imgRow = 'start';
+        for (var row = 1; row <= boardWidth; row++) {
+            // Check for columnRepeat
+            if ($('.' + row + '-' + col).attr('src') == imgColumn) {
+                countColumn++;
+            } else {
+                countColumn = 1;
+                imgColumn = $('.' + row + '-' + col).attr('src');
+            }
+            if (countColumn == 3) {
+                complete = addToArray(complete, (row - 2) + '-' + col);
+                complete = addToArray(complete, (row - 1) + '-' + col);
+                complete = addToArray(complete, row + '-' + col);
+            } else if (countColumn > 3) {
+                complete = addToArray(complete, row + '-' + col);
+            }
+            // Check for RowRepeat
+            if ($('.' + col + '-' + row).attr('src') == imgRow) {
+                countRow++;
+            } else {
+                countRow = 1;
+                imgRow = $('.' + col + '-' + row).attr('src');
+            }
+            if (countRow == 3) {
+                complete = addToArray(complete, col + '-' + (row - 2));
+                complete = addToArray(complete, col + '-' + (row - 1));
+                complete = addToArray(complete, col + '-' + row);
+            } else if (countRow > 3) {
+                complete = addToArray(complete, col + '-' + row);
+            }
+        }
+    }
+    if (complete.length == 0) {
+        enableDrag();
+    } else {
+        deleteCandy(complete, mult);
+    }
+}
+
+function deleteCandy(candyArr, mult) {
+    var speed = 300;
+    for (var i = 0; i < candyArr.length; i++) {
+        $('.' + candyArr[i]).animate({
+            opacity: 0
+        }, speed);
+        $('.' + candyArr[i]).animate({
+            opacity: 1
+        }, speed);
+        $('.' + candyArr[i]).animate({
+            opacity: 0
+        }, speed);
+        $('.' + candyArr[i]).animate({
+            opacity: 1
+        }, speed);
+        $('.' + candyArr[i]).animate({
+            opacity: 0
+        }, speed);
+        $('.' + candyArr[i]).animate({
+            opacity: 1
+        }, speed, function() {
+            $(this).remove();
+            refillCandy(mult + 1);
+        });
+        addPoints(10 * (i + 1) * mult);
+        console.log('10 * (' + i + ' + 1)' + ' * ' + mult + ' = ' + (10 * (i + 1) * mult));
+    }
+}
+
+function refillCandy(mult) {
+    var boardWidth = $('[class*="col-"]').length;
+    for (var col = 1; col <= boardWidth; col++) {
+        var childNum = $('.col-' + col)[0].childElementCount;
+        if (childNum < boardWidth) {
+            for (var i = 1; i <= childNum; i++) {
+                var newName = boardWidth + i - childNum;
+                var sliceClassName = $('.col-' + col + ' img:nth-child(' + i + ')')[0].className.slice(4)
+                $('.col-' + col + ' img:nth-child(' + i + ')')[0].className = (newName + '-' + col) + ' ' + sliceClassName;
+            }
+            var missing = boardWidth - childNum;
+            for (var i = missing; i >= 1; i--) {
+                var randomPic = Math.floor(Math.random() * 4) + 1;
+                $('.col-' + col).prepend('<img class="candy-img ' + i + '-' + col + '" src="image/' + randomPic + '.png" class="dulce" />');
+
+                var topStart = parseInt($('.' + i + '-' + col).css('height')) * -i;
+                animateCandy($('.' + i + '-' + col), topStart, 1, i - 1, col - 1);
+                setTimeout(function() {
+                    checkBoard(mult);
+                }, 1000);
+            }
+        }
+    }
+}
+
 function addMovement() {
     $('#movimientos-text').text(parseInt($('#movimientos-text').text()) + 1);
+}
+
+function addPoints(value) {
+    $('#score-text').text(parseInt($('#score-text').text()) + value);
+}
+
+function addToArray(arr, term) {
+    if (arr.indexOf(term) == -1) {
+        arr.push(term);
+    }
+    return arr;
+}
+
+function enableDrag() {
+    if ($('img[class*="candy-img"]').draggable('option', 'disabled')) {
+        console.log("enable");
+        $('img[class*="candy-img"]').draggable("enable");
+    }
+}
+
+function disableDrag() {
+    if (!$('img[class*="candy-img"]').draggable('option', 'disabled')) {
+        console.log('disable');
+        $('img[class*="candy-img"]').draggable({
+            disabled: true
+        });
+    }
 }
 //Document.Ready
 $(function() {
